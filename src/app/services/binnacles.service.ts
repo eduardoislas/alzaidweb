43269c12d08a1e4fc29ec83f7df462360125e712
eduardoislas <eislas22@gmail.com>
2020-05-03 05:25:41 -0700

    En desarrollo la bitacora de actividades paciente

--- src/app/services/binnacles.service.ts
+++ src/app/services/binnacles.service.ts
@@ -8,8 +8,8 @@ import { map, retry } from 'rxjs/operators';
 })
 export class BinnaclesService {
 
-    private url = 'https://alzaid.herokuapp.com';
-    // private url = 'http://74.208.247.106:3000';
+    // private url = 'https://alzaid.herokuapp.com';
+    private url = 'http://74.208.247.106:3000';
 
   constructor(private http: HttpClient) { }
 
